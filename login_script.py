db2ec5efa7a7d83a29e03d013c8671f0269edeff
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-08-26 17:47:24 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -75,8 +75,6 @@ async def main():
         is_logged_in = await login(username, password, panelnum)
 
         if is_logged_in:
-            now_utc = format_to_iso(datetime.utcnow())
-            now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
             success_message = f'serv00账号 {username} 登录成功！'
             print(success_message)
             send_pushplus_message(success_message)
